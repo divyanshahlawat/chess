30a3c3359fe3380df22b17a90aae6d63af2a1f40
Divyansh Ahlawat <Divyansh.A@moondive.co>
2025-11-06 00:42:42 +0530

    Server setup

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-//setup
+import express from "express"
+import http from "http"
+
+const app = express()
+const server = http.createServer(app)
+const PORT = 5000
+
+server.listen(PORT,(err)=>{
+    if(err) console.log(err)
+        console.log("Server is listening on Port", PORT)
+})
