58ed75cd349f3476257c3b0034dd52170fa606fc
Divyansh Ahlawat <Divyansh.A@moondive.co>
2025-11-13 13:07:30 +0530

    db setup

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 import express from "express"
 import http from "http"
+import Database from "./config/db.js"
+import dotenv from "dotenv"
+dotenv.config();
 
+const MONGO_URI = process.env.MONGO_URI;
+const dbName = process.env.dbName
 const app = express()
 const server = http.createServer(app)
 const PORT = 5000
+const db = new Database(MONGO_URI,dbName)
 
+db.connect()
 server.listen(PORT,(err)=>{
     if(err) console.log(err)
         console.log("Server is listening on Port", PORT)
